39c306afe3f1fa8c056ddb9e4ca8cf42832a2bae
BeastBreath <nividh.singh@gmail.com>
2022-07-22 14:17:26 -0700

    Finished Paired Up

--- Past USACO Problems/2016-17/Open 2017/Silver/Paired Up/Pairedup.cpp
+++ Past USACO Problems/2016-17/Open 2017/Silver/Paired Up/Pairedup.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+int max(int a, int b) {if (a > b) return a; else return b;}
+int min(int a, int b) {if (a < b) return a; else return b;}
+
+
 struct cows {
     int number, gal;
     cows() {}
@@ -13,8 +17,11 @@ struct cows {
 };
 
 int main() {
+    freopen("pairup.in","r",stdin);
+    freopen("pairup.out", "w", stdout);
     int N;
     cin >> N;
+
     cows herd[N];
     for (int i = 0; i < N; i++) {
         int a, b;
@@ -23,7 +30,17 @@ int main() {
     sort(herd, herd+N);
     int first = 0, second = N - 1, maxTime = 0;
     while (first < second) {
-        maxTime = max()
+        if (herd[first].number == 0) {
+            first++;
+        }
+        if (herd[second].number == 0) {
+            second--;
+        }
+        maxTime = max(maxTime, herd[first].gal + herd[second].gal);
+        int m = min(herd[first].number, herd[second].number);
+        herd[first].number -= m;
+        herd[second].number -= m;
     }
+    cout << maxTime;
 
 }
